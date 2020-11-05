96e57cbbfe6f3c70a866b36e56bd72af4537e952
orlando <orlandopaz645@gmail.com>
2020-11-05 14:58:37 -0800

    desafio hashes

---
+++ busqueda.rb
@@ -0,0 +1,32 @@
+ventas = {
+"Enero": 15000,
+"Febrero": 22000,
+"Marzo": 12000,
+"Abril": 17000,
+"Mayo": 81000,
+"Junio": 13000,
+"Julio": 21000,
+"Agosto": 41200,
+"Septiembre": 25000,
+"Octubre": 21500,
+"Noviembre": 91000,
+"Diciembre": 21000,
+}
+
+
+lista = ARGV
+lista.each do  |value| 
+    founded = false
+    ventas.each do |mes, precio|
+        if precio == value.to_i
+            founded = true
+            puts(mes)
+
+        end
+    end
+
+    if  founded == false
+        puts("no encontrado")
+    end     
+
+end
